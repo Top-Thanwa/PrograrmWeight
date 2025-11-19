6397cdb76ad1d62639595e8a1631ce58af829e7e
Top-Thanwa <phanetramtop@gmail.com>
2025-11-19 10:33:42 +0700

    Add project files.

---
+++ PrograrmWeight/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace PrograrmWeight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string gender = "";
+            int height = 0;
+            int weight = 0;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            Console.Write("Please input your gender M or F: ");
+            gender = Console.ReadLine();
+
+            Console.Write("Please fill you height(cm) : ");
+            height = Convert.ToInt16(Console.ReadLine());
+            if (gender == "M")
+            {
+                weight = height - 100;
+            }
+            else if (gender == "F")
+            {
+                weight = height - 110;
+            }
+            else Console.Write("Please input M or F: ");
+
+            Console.WriteLine("You ideal weight is " + weight.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to end program");
+            Console.ReadKey();
+        }
+    }
+}
